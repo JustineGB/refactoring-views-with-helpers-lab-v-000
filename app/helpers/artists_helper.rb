b23ddc57d7d87a7ad9c360e410ae9186237f8aff
Justine B <justinegayton@gmail.com>
2019-01-29 20:34:22 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,7 @@
 module ArtistsHelper
   def display_artist(song)
     if song.artist.empty? 
-     link_to name="Add Artist", edit_song_path
+     link_to name="Add Artist", edit_song_path(song)
     else 
      link_to artist.name, artist_path(artist)
     end 
